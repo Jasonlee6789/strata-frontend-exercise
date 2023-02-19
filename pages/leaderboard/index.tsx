5e0b423fd5004a73cd75fca34410b4dca36a253d
Jeff <li.jing@smartcompany.jp>
2023-02-19 17:02:12 +0900

    feat: Finish the username page  part.

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,14 +1,14 @@
 import { FC, useEffect, useState } from 'react'
 import axios from 'axios'
 import Link from 'next/link'
-
+import Image from 'next/image'
 const Leaderboard: FC = () => {
 	const [leaderboard, setLeaderboard] = useState([])
-
+	// const pictureUrl=
 	const fetchLeaderboard = async () => {
 		try {
 			const response = await axios.get('/api/leaderboard')
-			console.log('********', response.data)
+
 			setLeaderboard(response.data.leaderboard)
 		} catch (error) {
 			console.error(error)
@@ -24,16 +24,25 @@ const Leaderboard: FC = () => {
 		<>
 			<div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
 				<h1 className="text-4xl font-bold">Leaderboard</h1>
-				<p>TODO: complete.</p>
+
 				<ul>
 					{leaderboard.map((user: UserDetails) => (
 						<li
 							key={user.username}
 							className="flex items-center space-x-4"
 						>
-							<Link href={`/profile/${user.username}`}>
-								<img
-									className="w-20 h-20"
+							<Link
+								href={{
+									pathname: `/profile/${user.username}`,
+									query: {
+										profileImage: user.profileImage,
+										username: user.username,
+									},
+								}}
+							>
+								<Image
+									width="100"
+									height="100"
 									src={`${user.profileImage}`}
 									alt={user.username}
 								/>
--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,18 +1,69 @@
-import { FC } from "react"
-
-
-
+import { FC, useEffect, useState, useContext } from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+import { Button } from 'antd'
+import { LikeOutlined } from '@ant-design/icons'
+import { useRouter } from 'next/router'
+import React from 'react'
+const UserLikesContext = React.createContext<{
+	[userlike: string]: number
+}>({})
 const User: FC = () => {
+	const [userLikes, setUserLikes] = useState<{ [username: string]: number }>(
+		{}
+	)
+	const router = useRouter()
+	let { username, profileImage } = router.query
+	console.log('*****image 1=', profileImage)
+	//  in case refresh the  current page ,
+	if (!username || !profileImage) {
+		if (typeof window !== 'undefined') {
+			const params = new URLSearchParams(window.location.search)
+			username = params.get('username') as string
+			profileImage = params.get('profileImage') as string
+			console.log('********', profileImage)
+		}
+	}
+	const incrementUserLike = (username: string) => {
+		setUserLikes((prevLikes) => ({
+			...prevLikes,
+			[username]: (prevLikes[username] || 0) + 1,
+		}))
+	}
+	const [profileData, setProfileData] = useState<ProfileData>()
+	const fetchProfileData = async () => {
+		try {
+			const response = await axios.get(`/api/profile/${username}`)
 
-
-  return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">User</h1>
-      <p>TODO: complete</p>
-    </div>
-  </>)
-
+			setProfileData(response.data)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+	const handleLikeClick = () => {
+		// Increment the number of likes for this user
+		incrementUserLike(username as string)
+	}
+	useEffect(() => {
+		fetchProfileData()
+	}, [])
+	return (
+		<>
+			<div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
+				<h1 className="text-4xl font-bold">User</h1>
+				<Image
+					width="100"
+					height="100"
+					src={`${profileImage}`}
+					alt={profileData?.username || ''}
+				/>
+				<div>{profileData?.username}</div>
+				<Button icon={<LikeOutlined />} onClick={handleLikeClick}>
+					Like {userLikes[username as string]}
+				</Button>
+			</div>
+		</>
+	)
 }
-
 
 export default User
