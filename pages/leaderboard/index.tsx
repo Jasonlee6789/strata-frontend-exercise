6b500c8acfbca39c5d1c466a07f67ec1641ac7a1
Jeff <li.jing@smartcompany.jp>
2023-02-19 23:07:48 +0900

    feat: Finish the responsive css .

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -21,14 +21,14 @@ const Leaderboard: FC = () => {
 	}, [])
 	return (
 		<>
-			<div className="w-full h-80 mb-100 flex flex-col items-center justify-center space-y-12">
+			<div className="w-full h-160 mb-100 flex flex-col items-center justify-center space-y-12 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32">
 				<h1 className="text-4xl font-bold">Leaderboard</h1>
 
-				<ul>
+				<ul className="space-y-4 ">
 					{leaderboard.map((user: UserDetails) => (
 						<li
 							key={user.username}
-							className="flex items-center space-x-4"
+							className="flex items-center space-x-4  cursor-pointer hover:bg-gray-100 p-4 rounded-lg transition-colors duration-200 ease-in-out"
 						>
 							<Link
 								href={{
@@ -39,18 +39,23 @@ const Leaderboard: FC = () => {
 									},
 								}}
 							>
-								<Image
-									width="100"
-									height="100"
-									src={`${user.profileImage}`}
-									alt={user.username}
-								/>
-								<div>{user.username}</div>
+								<div className="flex items-center space-x-4">
+									<Image
+										width="100"
+										height="100"
+										src={`${user.profileImage}`}
+										alt={user.username}
+										className="rounded-full object-cover"
+									/>
+									<div className="text-lg font-semibold">
+										{user.username}
+									</div>
+								</div>
 							</Link>
 						</li>
 					))}
 				</ul>
-				<p>
+				<p className="text-center">
 					All leaderboard entries should be links to user profile
 					page.
 				</p>
